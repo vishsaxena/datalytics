213593fe2cfcf8164f2a6c3eddd1fe6b0e9534d9
Vishal Saxena <mr.saxena.vishal@gmail.com>
2023-02-17 12:31:59 +0530

    started the go routines

--- api/main.go
+++ api/main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func someFunc(str string) {
+	fmt.Println(str)
+}
+
 func main() {
+	go someFunc("First Method Call")
+	go someFunc("Second Method Call")
+	go someFunc("Third Method Call")
 	fmt.Println("Hello Datalytics API...")
 }
